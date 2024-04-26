86769427eaf5c9b077ec87aae126d380aea9676a
Wouter Massie <wouter.massie@acolad.com>
2024-04-26 12:23:43 +0200

    Added Qwik + refactored langswitcher

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 
 import tailwind from "@astrojs/tailwind";
+import qwikdev from '@qwikdev/astro';
 
 // https://astro.build/config
 export default defineConfig({
@@ -10,6 +11,7 @@ export default defineConfig({
   integrations: [
     mdx(), 
     sitemap(), 
+    qwikdev(),
     tailwind({
        nesting: true,
     })
---
+++ src/components/LanguageMenu.tsx
@@ -0,0 +1,45 @@
+import { component$, useSignal, $, useOnDocument } from '@builder.io/qwik';
+import { languages } from '../i18n/consts';
+import iconWorld from '../assets/icons/world.svg'
+import Link from './ui/Link.astro';
+
+interface languagesProps {
+    currentLang: string
+}
+
+const LanguageMenu = component$((props: languagesProps) => {
+    const isVisible = useSignal(false);
+
+    useOnDocument(
+        "click",
+        $((event: Event) => {
+            if(isVisible.value === true) {
+                isVisible.value = false;
+            }
+        })
+    );
+    
+    return (
+        <>
+            {isVisible.value ? (
+                <ul class="absolute bottom-0 p-4 text-white border border-blue-200 shadow-sm -right-2 l backdrop-blur-sm bg-primary/70 rounded-xl">
+                    {Object.entries(languages).map(([lang, label]) => (
+                        <li>
+                            <a class="text-white hover:underline focus-visible:outline-none focus-visible:rounded focus-visible:ring-2 focus-visible:ring-tertiary" href={`/${lang}/`}>{label}</a>
+                        </li>
+                    ))}
+                </ul>
+            ) : null}
+            <button 
+            onClick$={() => {
+                isVisible.value = !isVisible.value;
+            }} 
+            class="inline-flex gap-2 text-white focus-visible:outline-none focus-visible:rounded focus-visible:ring-2 focus-visible:ring-tertiary">
+                <img class="w-6 h-6" src={iconWorld.src} alt="world" />
+                {languages[props.currentLang]}
+            </button>
+        </>
+    );
+});
+ 
+export default LanguageMenu;
