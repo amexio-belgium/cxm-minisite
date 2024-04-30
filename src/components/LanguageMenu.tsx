af73e5d46d4da543d017d1cf54f030adf1c31c35
Geert Melotte <geert.melotte@acolad.com>
2024-04-30 13:34:13 +0200

    Close language popup on escape Close language menu when focus leaves language menu Add aria-current to active language

--- src/components/LanguageMenu.tsx
+++ src/components/LanguageMenu.tsx
@@ -1,4 +1,4 @@
-import { component$, useSignal, $, useOnDocument } from '@builder.io/qwik';
+import { component$, useId, useSignal, $, useOnDocument } from '@builder.io/qwik';
 import { languages } from '../i18n/consts';
 import iconWorld from '../assets/icons/world.svg'
 
@@ -8,6 +8,10 @@ interface languagesProps {
 
 const LanguageMenu = component$((props: languagesProps) => {
     const isVisible = useSignal(false);
+    const id = useId();
+    const languageTriggerId = `langTrigger-${id}`;
+    const languageMenuId = `langMenu-${id}`;
+
 
     useOnDocument(
         "click",
@@ -17,23 +21,38 @@ const LanguageMenu = component$((props: languagesProps) => {
             }
         })
     );
+
+    const handleKeyUp = $((event: KeyboardEvent) => {
+        if (event.key === 'Escape' && isVisible.value === true) {
+            isVisible.value = false;
+            document.getElementById(languageTriggerId)?.focus();
+          }
+    });
+
+    const handleLanguageMenuBlur = $((event: FocusEvent) => {
+        const languageMenu = document.getElementById(languageMenuId);
+        if (!languageMenu?.contains(event.relatedTarget as Node)) {
+            isVisible.value = false;
+        }
+    });
     
     return (
         <>
-            <button 
+            <button id="{languageTriggerId}"
             onClick$={() => {
                 isVisible.value = !isVisible.value;
             }} 
             aria-expanded={isVisible.value}
+            type="button"
             class="inline-flex gap-2 text-white focus-visible:outline-none focus-visible:rounded focus-visible:ring-2 focus-visible:ring-tertiary">
                 <img class="w-6 h-6" src={iconWorld.src} alt="" />
                 {languages[props.currentLang]}
             </button>
             {isVisible.value ? (
-                <ul class="absolute bottom-0 p-4 text-white border border-blue-200 shadow-sm -right-2 l backdrop-blur-sm bg-primary/70 rounded-xl">
+                <ul id={languageMenuId} window:onKeyUp$={handleKeyUp}  document:onBlur$={handleLanguageMenuBlur} class="absolute bottom-0 p-4 text-white border border-blue-200 shadow-sm -right-2 l backdrop-blur-sm bg-primary/70 rounded-xl">
                     {Object.entries(languages).map(([lang, label]) => (
                         <li>
-                            <a class="text-white hover:underline focus-visible:outline-none focus-visible:rounded focus-visible:ring-2 focus-visible:ring-tertiary" href={`/${lang}/`} hreflang={`${lang}`} lang={`${lang}`}>{label}</a>
+                            <a aria-current={props.currentLang == lang} class="text-white hover:underline focus-visible:outline-none focus-visible:rounded focus-visible:ring-2 focus-visible:ring-tertiary" href={`/${lang}/`} hreflang={`${lang}`} lang={`${lang}`}>{label}</a>
                         </li>
                     ))}
                 </ul>
