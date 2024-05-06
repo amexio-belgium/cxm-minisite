0f65c0002744bad8537490f08e82d28004e8c5c5
Wouter Massie <wouter.massie@acolad.com>
2024-05-06 17:50:28 +0200

    Fixed icons

--- src/components/FaqQuestion.tsx
+++ src/components/FaqQuestion.tsx
@@ -41,19 +41,19 @@ const FaqQuestion = component$((props: Props) => {
     
     return (
         <div class="border-b border-gray-500 justify-start items-start gap-2 inline-flex flex-col py-6">
-            <div class="flex justify-between w-full gap-2" id={faqTriggerId}
+            <div class="flex justify-between items-center w-full gap-4" id={faqTriggerId}
                 onClick$={() => {isVisible.value = !isVisible.value;}} 
                 aria-expanded={isVisible.value}>
                 <b class="text-lg">
                     {props.headline}
                 </b>
                 {isVisible.value ? 
-                    <svg class="icon-open"  width="34" height="34" viewBox="0 0 34 34" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <svg class="icon-open  w-10 h-10 flex-shrink-0"  width="34" height="34" viewBox="0 0 34 34" fill="none" xmlns="http://www.w3.org/2000/svg">
                         <rect width="34" height="34" rx="17" fill="#FFC256"/>
                         <path d="M11.75 17.75V16.25H22.25V17.75H11.75Z" fill="#0B1215"/>
                     </svg>  
                 :   
-                    <svg class="icon-closed" width="34" height="34" viewBox="0 0 34 34" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <svg class="icon-closed w-10 h-10  flex-shrink-0" width="34" height="34" viewBox="0 0 34 34" fill="none" xmlns="http://www.w3.org/2000/svg">
                         <rect width="34" height="34" rx="17" fill="#FFC256"/>
                         <path d="M16.25 17.75H11.75V16.25H16.25V11.75H17.75V16.25H22.25V17.75H17.75V22.25H16.25V17.75Z" fill="#0B1215"/>
                     </svg>
