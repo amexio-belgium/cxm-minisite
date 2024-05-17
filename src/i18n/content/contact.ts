3cfd671ab1b853a3f0d218f08963e8a8ba998251
Wouter Massie <wouter.massie@acolad.com>
2024-05-17 12:06:35 +0200

    Added GTM + Prose

--- src/i18n/content/contact.ts
+++ src/i18n/content/contact.ts
@@ -4,6 +4,9 @@ export const contact = {
         'contact.seoDescription': "Enter a concise summary of the page content here, highlighting key information and benefits. Include relevant keywords and end with a clear call to action.",
         'hero.headline': 'Contact',
         'hero.subheadline': 'A streamlined content lifecycle is at the core of your Customer Experience Management (CXM) process, and goes beyond the implementation of a Content Management System (CMS). Therefore, we provide a broad spectrum of services to help you meet concrete business outcomes.',
+        'highlight.headline': 'Come work for us',
+        'highlight.text': 'Lorem ipsum dolor sit amet massa dapibus orci lobortis. Fusce malesuada leo senectus at libero aliqua etiam.Lorem ipsum dolor sit amet massa dapibus orci lobortis. Fusce malesuada leo senectus at libero aliqua etiam.',
+        'highlight.button': 'Our jobs',
     },
     fr: {
         'hero.headline': 'Turning your content <mark>into value</mark>',
