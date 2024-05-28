11597582537733a822e9beb7ddb97f62d555a3ad
Wouter Massie <wouter.massie@acolad.com>
2024-05-28 15:14:41 +0200

    images to webp + nl / fr translations

--- src/i18n/content/blog.ts
+++ src/i18n/content/blog.ts
@@ -6,9 +6,15 @@ export const blog = {
         'hero.subheadline': 'A streamlined content lifecycle is at the core of your Customer Experience Management (CXM) process, and goes beyond the implementation of a Content Management System (CMS). Therefore, we provide a broad spectrum of services to help you meet concrete business outcomes.',
     },
     fr: {
-        'hero.headline': 'Turning your content <mark>into value</mark>',
+        'blog.title': "Points de vue - AmeXio Fuse",
+        'blog.seoDescription': "Entrez un résumé concis du contenu de la page ici, mettant en évidence les informations clés et les avantages. Incluez des mots-clés pertinents et terminez par un appel à l'action clair.",
+        'hero.headline': 'Points de vue',
+        'hero.subheadline': "Un cycle de vie du contenu rationalisé est au cœur de votre processus de gestion de l'expérience client (CXM) et va au-delà de la mise en œuvre d'un système de gestion de contenu (CMS). C'est pourquoi nous proposons une large gamme de services pour vous aider à atteindre des résultats commerciaux concrets.",
     },
     nl: {
-        'hero.headline': 'Turning your content <mark>into value</mark>',
+        'blog.title': "Inzichten - AmeXio Fuse",
+        'blog.seoDescription': "Voer hier een beknopte samenvatting van de pagina-inhoud in, waarbij belangrijke informatie en voordelen worden benadrukt. Inclusief relevante zoekwoorden en eindig met een duidelijke oproep tot actie.",
+        'hero.headline': 'Inzichten',
+        'hero.subheadline': 'Een gestroomlijnde levenscyclus van inhoud staat centraal in uw Customer Experience Management (CXM) -proces en gaat verder dan de implementatie van een Content Management System (CMS). Daarom bieden we een breed scala aan diensten om u te helpen concrete bedrijfsresultaten te behalen.',
     },
 } as const;
