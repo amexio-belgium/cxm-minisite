41f5930e4101c4a2f3d9cae80807e17488b70c04
Jan Lemmens <>
2024-06-04 07:58:35 +0200

    Services content.

--- src/i18n/content/services.ts
+++ src/i18n/content/services.ts
@@ -3,7 +3,7 @@ export const services = {
     en: {
         'services.title': "Services - AmeXio Fuse",
         'services.seoDescription': "Enter a concise summary of the page content here, highlighting key information and benefits. Include relevant keywords and end with a clear call to action.",
-        'hero.headline': 'Turning your content <mark>into value</mark>',
+        'hero.headline': 'Services',
         'hero.subheadline': 'A streamlined <strong>content lifecycle</strong> is at the core of your Customer Experience Management (CXM) process, and goes beyond the implementation of a Content Management System (CMS). Therefore, we provide a broad spectrum of services to help you meet concrete <strong>business outcomes</strong>.',
         'overview.headline': 'We can help you to',
         'overview.backupcta': 'Discover more about this service',
