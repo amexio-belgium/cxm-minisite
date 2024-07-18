de4024d026bf9ac8a0c789d10ad7b761e38ec7c6
HAKOBYAN, Davit <davit.hakobyan@acolad.com>
2024-07-18 13:59:41 +0200

    partytown test

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,36 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
-
 import tailwind from "@astrojs/tailwind";
 import qwikdev from '@qwikdev/astro';
+
+import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://dreamy-trifle-7f42a6.netlify.app',
-  integrations: [
-    mdx(), 
-    sitemap(), 
-    qwikdev(),
-    tailwind({
-       nesting: true,
-    })
-  ]
+  integrations: [mdx(), sitemap(), qwikdev(), tailwind({
+    nesting: true
+  }), partytown({
+    config: {
+      resolveUrl(url, location, type) {
+        const proxyMap = {
+          'https://consent.cookiebot.com': 'http://localhost:4321/cookiebot',
+        }
+        url.hostname = proxyMap[url.hostname] || url.hostname;
+        return url;
+      },
+      forward: ['datalayer.push']
+    }
+  })],
+  vite: {
+    server: {
+      proxy: {
+        '/cookiebot': {
+          tager: 'https://consent.cookiebot.com',
+          changeOrigin: true,
+        }
+      }
+    }
+  }
 });
