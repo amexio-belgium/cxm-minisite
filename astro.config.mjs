297c37e7cfd0cfa2f7dd5933b09da25f70da9510
HAKOBYAN, Davit <davit.hakobyan@acolad.com>
2024-07-18 17:17:10 +0200

    Fix cookiebot

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,8 @@ import tailwind from "@astrojs/tailwind";
 import qwikdev from '@qwikdev/astro';
 
 import partytown from "@astrojs/partytown";
-
+import { loadEnv } from "vite";
+const { SITE_URL } = loadEnv(process.env.NODE_ENV, process.cwd(), "");
 // https://astro.build/config
 export default defineConfig({
   site: 'https://partytown-test--dreamy-trifle-7f42a6.netlify.app',
@@ -13,14 +14,36 @@ export default defineConfig({
     nesting: true
   }), partytown({
     config: {
-      // resolveUrl(url, location, type) {
-      //   const proxyMap = {
-      //     'https://consent.cookiebot.com/uc.js': 'http://localhost:4321/cookiebot',
-      //     'https://consentcdn.cookiebot.com': 'http://localhost:4321/cookiebotcdn'
-      //   }
-      //   url.hostname = proxyMap[url.hostname] || url.hostname;
-      //   return url;
-      // },
+      debug: false,
+      resolveUrl(url, location, type) {
+        const proxyMap = {
+          'consent.cookiebot.com': '/cookiebot',
+          'consentcdn.cookiebot.com': '/cookiebotcdn',
+          'consentcdn.cookiebot.eu': '/cookiebotcdneu'
+        }
+        
+        console.log('siteurl: ' + SITE_URL);
+        
+        if(proxyMap[url.hostname] && proxyMap[url.hostname] !== ''){
+          const newUrl = new URL(SITE_URL);
+          newUrl.pathname = proxyMap[url.hostname] + url.pathname;
+          newUrl.search = url.search;
+          newUrl.searchParams = url.searchParams;
+          return newUrl;
+        }
+        
+        if(url.pathname.startsWith('/3f5a3037-fd04-4d20-9b59-cd8759bfacf9/') && url.hostname === 'partytown-test--dreamy-trifle-7f42a6.netlify.app'){
+          const newUrl = new URL(SITE_URL);
+          newUrl.pathname = '/cookiebot' + url.pathname;
+          newUrl.search = url.search;
+          newUrl.searchParams = url.searchParams;
+          return newUrl;
+        }
+        
+        return url;
+
+      },
+      
       forward: ['datalayer.push']
     }
   })],
@@ -32,7 +55,6 @@ export default defineConfig({
           changeOrigin: true,
           rewrite: (path) => {
             const newpath = path.slice(10)
-            console.log(newpath)
             return newpath
           },
         },
@@ -41,21 +63,17 @@ export default defineConfig({
           changeOrigin: true,
           rewrite: (path) => {
             const newpath = path.slice(13)
-            console.log('cookiebotcdn.com: ' + newpath)
             return newpath
           },
         },
         '/cookiebotcdneu/': {
-          target: 'https://consentcdn.cookiebot.com',
+          target: 'https://consentcdn.cookiebot.eu',
           changeOrigin: true,
           rewrite: (path) => {
             const newpath = path.slice(15)
-            console.log('hi')
-            console.log(newpath)
             return newpath
           },
         },
-
       }
     }
   }
