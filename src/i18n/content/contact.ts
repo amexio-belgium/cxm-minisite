70317b56c8c56af4b0a59ebd6e17c0025a99988a
HAKOBYAN, Davit <davit.hakobyan@acolad.com>
2024-08-09 14:19:06 +0200

    Reformat latest changes

--- src/i18n/content/contact.ts
+++ src/i18n/content/contact.ts
@@ -1,35 +1,44 @@
 export const contact = {
-    en: {
-        'contact.title': "Contact - AmeXio Fuse",
-        'contact.seoDescription': "Enter a concise summary of the page content here, highlighting key information and benefits. Include relevant keywords and end with a clear call to action.",
-        'hero.headline': '<mark>Contact</mark> us',
-        'hero.subheadline': 'We’d love to talk about your content challenges! Let’s explore how we can remove obstacles from your content lifecycle and help your team manage and scale operations more effectively.<br /><br />Are you ready to unlock more <strong><mark>value</mark></strong> from your content?',
-        'form.headline': 'Let\'s discuss your content challenge',
-        'highlight.headline': 'Visit us on-site',
-        'highlight.text': 'Drop by one of our offices and meet with our teams in person!',
-        'highlight.button': 'Discover our office locations',
-        'highlight.link': 'https://www.amexio.fr/en/contact-en/',
-    },
-    fr: {
-        'contact.title': "Contact - AmeXio Fuse",
-        'contact.seoDescription': "Entrez un résumé concis du contenu de la page ici, mettant en évidence les informations clés et les avantages. Incluez des mots-clés pertinents et terminez par un appel à l'action clair.",
-        'hero.headline': 'Contact',
-        'hero.subheadline': "Un cycle de vie du contenu rationalisé est au cœur de votre processus de gestion de l'expérience client (CXM) et va au-delà de la mise en œuvre d'un système de gestion de contenu (CMS). C'est pourquoi nous proposons une large gamme de services pour vous aider à atteindre des résultats commerciaux concrets.",
-        'form.headline': 'FR : contact form headline',
-        'highlight.headline': 'Venez travailler pour nous',
-        'highlight.text': 'Lorem ipsum dolor sit amet massa dapibus orci lobortis. Fusce malesuada leo senectus at libero aliqua etiam.Lorem ipsum dolor sit amet massa dapibus orci lobortis. Fusce malesuada leo senectus at libero aliqua etiam.',
-        'highlight.button': 'Visit the AmeXio Group website',
-        'highlight.link': 'https://www.amexio.fr/en/contact-en/',
-    },
-    nl: {
-        'contact.title': "Contact - AmeXio Fuse",
-        'contact.seoDescription': "Voer hier een beknopte samenvatting van de pagina-inhoud in, waarbij belangrijke informatie en voordelen worden benadrukt. Inclusief relevante zoekwoorden en eindig met een duidelijke oproep tot actie.",
-        'hero.headline': 'Contact',
-        'hero.subheadline': 'Een gestroomlijnde levenscyclus van inhoud staat centraal in uw Customer Experience Management (CXM) -proces en gaat verder dan de implementatie van een Content Management System (CMS). Daarom bieden we een breed scala aan diensten om u te helpen concrete bedrijfsresultaten te behalen.',
-        'form.headline': 'NL : contact form headline',
-        'highlight.headline': 'Kom bij ons werken',
-        'highlight.text': 'Lorem ipsum dolor sit amet massa dapibus orci lobortis. Fusce malesuada leo senectus at libero aliqua etiam.Lorem ipsum dolor sit amet massa dapibus orci lobortis. Fusce malesuada leo senectus at libero aliqua etiam.',
-        'highlight.button': 'Visit the AmeXio Group website',
-        'highlight.link': 'https://www.amexio.fr/contact/',
-    },
+  en: {
+    "contact.title": "Contact - AmeXio Fuse",
+    "contact.seoDescription":
+      "Enter a concise summary of the page content here, highlighting key information and benefits. Include relevant keywords and end with a clear call to action.",
+    "hero.headline": "<mark>Contact</mark> us",
+    "hero.subheadline":
+      "We’d love to talk about your content challenges! Let’s explore how we can remove obstacles from your content lifecycle and help your team manage and scale operations more effectively.<br /><br />Are you ready to unlock more <strong><mark>value</mark></strong> from your content?",
+    "form.headline": "Let's discuss your content challenge",
+    "highlight.headline": "Visit us on-site",
+    "highlight.text":
+      "Drop by one of our offices and meet with our teams in person!",
+    "highlight.button": "Discover our office locations",
+    "highlight.link": "https://www.amexio.fr/en/contact-en/",
+  },
+  fr: {
+    "contact.title": "Contact - AmeXio Fuse",
+    "contact.seoDescription":
+      "Entrez un résumé concis du contenu de la page ici, mettant en évidence les informations clés et les avantages. Incluez des mots-clés pertinents et terminez par un appel à l'action clair.",
+    "hero.headline": "Contact",
+    "hero.subheadline":
+      "Un cycle de vie du contenu rationalisé est au cœur de votre processus de gestion de l'expérience client (CXM) et va au-delà de la mise en œuvre d'un système de gestion de contenu (CMS). C'est pourquoi nous proposons une large gamme de services pour vous aider à atteindre des résultats commerciaux concrets.",
+    "form.headline": "FR : contact form headline",
+    "highlight.headline": "Venez travailler pour nous",
+    "highlight.text":
+      "Lorem ipsum dolor sit amet massa dapibus orci lobortis. Fusce malesuada leo senectus at libero aliqua etiam.Lorem ipsum dolor sit amet massa dapibus orci lobortis. Fusce malesuada leo senectus at libero aliqua etiam.",
+    "highlight.button": "Visit the AmeXio Group website",
+    "highlight.link": "https://www.amexio.fr/en/contact-en/",
+  },
+  nl: {
+    "contact.title": "Contact - AmeXio Fuse",
+    "contact.seoDescription":
+      "Voer hier een beknopte samenvatting van de pagina-inhoud in, waarbij belangrijke informatie en voordelen worden benadrukt. Inclusief relevante zoekwoorden en eindig met een duidelijke oproep tot actie.",
+    "hero.headline": "Contact",
+    "hero.subheadline":
+      "Een gestroomlijnde levenscyclus van inhoud staat centraal in uw Customer Experience Management (CXM) -proces en gaat verder dan de implementatie van een Content Management System (CMS). Daarom bieden we een breed scala aan diensten om u te helpen concrete bedrijfsresultaten te behalen.",
+    "form.headline": "NL : contact form headline",
+    "highlight.headline": "Kom bij ons werken",
+    "highlight.text":
+      "Lorem ipsum dolor sit amet massa dapibus orci lobortis. Fusce malesuada leo senectus at libero aliqua etiam.Lorem ipsum dolor sit amet massa dapibus orci lobortis. Fusce malesuada leo senectus at libero aliqua etiam.",
+    "highlight.button": "Visit the AmeXio Group website",
+    "highlight.link": "https://www.amexio.fr/contact/",
+  },
 } as const;
