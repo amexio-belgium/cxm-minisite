8a4b6645d68dc3745dbe2cf3f93a40b6bc623a1b
HAKOBYAN, Davit <davit.hakobyan@acolad.com>
2024-08-23 15:08:50 +0200

    Fix breadcrumbs, rename services and blogs

--- src/helpers.ts
+++ src/helpers.ts
@@ -1,10 +1,14 @@
-export const parseHref = (href: string | URL | undefined) => {
+export const parseHref = (href: string | URL | undefined | null) => {
   if (href) {
     const hrefString = href.toString();
     return hrefString.startsWith("http://") ||
       hrefString.startsWith("https://") ||
       hrefString.startsWith("www.") ||
-      hrefString.startsWith("#")
+      hrefString.startsWith("#") ||
+      hrefString.startsWith("/fuse/") ||
+      hrefString.startsWith("fuse/") ||
+      hrefString.startsWith("./") ||
+      hrefString.startsWith("../")
       ? href
       : import.meta.env.PUBLIC_ASTRO_BASE_PATH + href;
   }
