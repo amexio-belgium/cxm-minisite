8866faa2be6fcd3824abd607c9d0b0c8d6c575c5
HAKOBYAN, Davit <davit.hakobyan@acolad.com>
2024-09-18 14:32:28 +0200

    Update partytown domainname

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
 import partytown from "@astrojs/partytown";
 import tailwind from "@astrojs/tailwind";
 import { loadEnv } from "vite";
@@ -27,35 +27,36 @@ export default defineConfig({
         debug: false,
         resolveUrl(url, location, type) {
           const proxyMap = {
-            'consent.cookiebot.com': '/cookiebot',
-            'consentcdn.cookiebot.com': '/cookiebotcdn',
-            'consentcdn.cookiebot.eu': '/cookiebotcdneu'
-          }
+            "consent.cookiebot.com": "/cookiebot",
+            "consentcdn.cookiebot.com": "/cookiebotcdn",
+            "consentcdn.cookiebot.eu": "/cookiebotcdneu",
+          };
 
-
-          if(proxyMap[url.hostname] && proxyMap[url.hostname] !== ''){
-            const newUrl = new URL('https://partytown-test--dreamy-trifle-7f42a6.netlify.app');
+          if (proxyMap[url.hostname] && proxyMap[url.hostname] !== "") {
+            const newUrl = new URL("https://partytown-fuse.netlify.app");
             newUrl.pathname = proxyMap[url.hostname] + url.pathname;
             newUrl.search = url.search;
             newUrl.searchParams = url.searchParams;
             return newUrl;
           }
 
-          if(url.pathname.startsWith('/3f5a3037-fd04-4d20-9b59-cd8759bfacf9/') && url.hostname === 'partytown-test--dreamy-trifle-7f42a6.netlify.app'){
-            const newUrl = new URL('https://partytown-test--dreamy-trifle-7f42a6.netlify.app');
-            newUrl.pathname = '/cookiebot' + url.pathname;
+          if (
+            url.pathname.startsWith("/3f5a3037-fd04-4d20-9b59-cd8759bfacf9/") &&
+            url.hostname === "partytown-fuse.netlify.app"
+          ) {
+            const newUrl = new URL("https://partytown-fuse.netlify.app");
+            newUrl.pathname = "/cookiebot" + url.pathname;
             newUrl.search = url.search;
             newUrl.searchParams = url.searchParams;
             return newUrl;
           }
 
           return url;
-
         },
 
-        forward: ['datalayer.push']
-      }
-    })
+        forward: ["datalayer.push"],
+      },
+    }),
   ],
   base: basePath,
   vite: {
@@ -68,31 +69,31 @@ export default defineConfig({
     },
     server: {
       proxy: {
-        '/cookiebot/': {
-          target: 'https://consent.cookiebot.com',
+        "/cookiebot/": {
+          target: "https://consent.cookiebot.com",
           changeOrigin: true,
           rewrite: (path) => {
-            const newpath = path.slice(10)
-            return newpath
+            const newpath = path.slice(10);
+            return newpath;
           },
         },
-        '/cookiebotcdn/': {
-          target: 'https://consentcdn.cookiebot.com',
+        "/cookiebotcdn/": {
+          target: "https://consentcdn.cookiebot.com",
           changeOrigin: true,
           rewrite: (path) => {
-            const newpath = path.slice(13)
-            return newpath
+            const newpath = path.slice(13);
+            return newpath;
           },
         },
-        '/cookiebotcdneu/': {
-          target: 'https://consentcdn.cookiebot.eu',
+        "/cookiebotcdneu/": {
+          target: "https://consentcdn.cookiebot.eu",
           changeOrigin: true,
           rewrite: (path) => {
-            const newpath = path.slice(15)
-            return newpath
+            const newpath = path.slice(15);
+            return newpath;
           },
         },
-      }
-    }
+      },
+    },
   },
 });
