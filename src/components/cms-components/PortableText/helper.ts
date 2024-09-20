52ce64b093a0a16e06024f5d8f9bc1a3f349c312
HAKOBYAN, Davit <davit.hakobyan@acolad.com>
2024-09-20 22:52:19 +0200

    Add portablemark component to fix highlight and link functionality in portabletext

--- src/components/cms-components/PortableText/helper.ts
+++ src/components/cms-components/PortableText/helper.ts
@@ -6,7 +6,7 @@ import PortableH6 from "@components/cms-components/PortableText/PortableH6.astro
 import PortableImage from "@components/cms-components/PortableText/PortableImage.astro";
 import PortableYoutube from "@components/cms-components/PortableText/PortableYoutube.astro";
 import type { SomePortableTextComponents } from "astro-portabletext/types";
-
+import PortableMark from "./PortableMark.astro";
 export const defaultComponents: SomePortableTextComponents = {
   type: {
     image: PortableImage,
@@ -19,4 +19,5 @@ export const defaultComponents: SomePortableTextComponents = {
     h5: PortableH5,
     h6: PortableH6,
   },
+  mark: PortableMark,
 };
