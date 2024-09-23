b5cc628a09caa9228dc4a80041aaeddbc6a2a18d
DavitAM <71630103+DavithkbY@users.noreply.github.com>
2024-09-23 13:04:20 +0200

    Add portablemark component to fix highlight and link functionality in portabletext (#140)
    Co-authored-by: HAKOBYAN, Davit <davit.hakobyan@acolad.com>

--- src/lib/sanityHelpers.ts
+++ src/lib/sanityHelpers.ts
@@ -16,6 +16,9 @@ export const getHrefFromLinkObject = (
     href = `${BASE_PATH}/${language}/${linkObject.internalLink.metadata?.slug?.current}`;
   } else if (linkObject?.type === "external") {
     href = linkObject.url || "";
+    if (!href.startsWith("https://") && !href.startsWith("http://")) {
+      href = "https://" + href;
+    }
   } else if (linkObject?.type === "email") {
     href = `mailto:${linkObject.email}` || "";
   } else if (linkObject?.type === "phone") {
