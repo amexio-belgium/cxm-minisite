514b7dde85f62fadb6d8c644dd13bb2761c9515f
Terence Koch <terence.koch@amplexor.com>
2024-10-02 17:28:34 +0200

    CW-146 fix sitemap make generic function for client fetches for sanity content and filter out no indexed pages for sitemap fix script

--- src/scripts/generateRssFiles.js
+++ src/scripts/generateRssFiles.js
@@ -1,7 +1,7 @@
 import "dotenv/config";
-import { createClient } from "@sanity/client";
 import * as fs from "node:fs";
 import RSS from "rss";
+import { sanityContentQuery } from "../scripts/sanityContentQuery.js";
 
 const PUBLIC_ASTRO_BASE_PATH = process.env.PUBLIC_ASTRO_BASE_PATH;
 const SANITY_VISUAL_EDITING_ENABLED = process.env.SANITY_VISUAL_EDITING_ENABLED;
@@ -9,50 +9,14 @@ const SANITY_VISUAL_EDITING_ENABLED = process.env.SANITY_VISUAL_EDITING_ENABLED;
 if (SANITY_VISUAL_EDITING_ENABLED !== "true") {
   const basePath = PUBLIC_ASTRO_BASE_PATH || "";
 
-  const client = createClient({
-    projectId: process.env.SANITY_STUDIO_PROJECT_ID,
-    dataset: process.env.SANITY_STUDIO_DATASET,
-    useCdn: false, // set to `false` to bypass the edge cache
-    apiVersion: "2023-05-03", // use current date (YYYY-MM-DD) to target the latest API version
-  });
-
-  const visualEditingEnabled =
-    process.env.SANITY_VISUAL_EDITING_ENABLED === "true";
-  const token = process.env.SANITY_API_READ_TOKEN;
-
-  async function querySanityData({ query, params }) {
-    if (visualEditingEnabled && !token) {
-      throw new Error(
-        "The `SANITY_API_READ_TOKEN` environment variable is required in Draft Mode.",
-      );
-    }
-
-    const perspective = visualEditingEnabled ? "previewDrafts" : "published";
-
-    const { result, resultSourceMap } = await client.fetch(
-      query,
-      params ?? {},
-      {
-        filterResponse: false,
-        perspective,
-        resultSourceMap: visualEditingEnabled ? "withKeyArraySelector" : false,
-        stega: false,
-        ...(visualEditingEnabled ? { token } : {}),
-        useCdn: false,
-      },
-    );
-
-    return {
-      data: result,
-      sourceMap: resultSourceMap,
-      perspective,
-    };
-  }
-
-  const locales = ["en", "nl", "fr"];
+  const locales = [
+    "en",
+    // "nl",
+    // "fr"
+  ];
 
   for (const locale of locales) {
-    const { data: siteConfig } = await querySanityData({
+    const { data: siteConfig } = await sanityContentQuery({
       query: `*[_type == "siteConfig" && language == $language][0]{
   ...,
   homePage->{
@@ -73,7 +37,7 @@ if (SANITY_VISUAL_EDITING_ENABLED !== "true") {
       params: { language: locale },
     });
 
-    const { data: posts } = await querySanityData({
+    const { data: posts } = await sanityContentQuery({
       query: `*[_type == "blogPost" && language == $language]{
   "title": metadata.title,
   "slug": metadata.slug.current,
