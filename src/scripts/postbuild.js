5d24e44b072016d965911baee078bb9853dc42c0
HAKOBYAN, Davit <davit.hakobyan@acolad.com>
2024-10-17 14:50:51 +0200

    Fix broken pipeline

--- src/scripts/postbuild.js
+++ src/scripts/postbuild.js
@@ -1,14 +1,20 @@
 import fs from "node:fs";
 import * as fsExtra from "fs-extra";
 import "dotenv/config";
+import { VISUAL_EDITING_ENABLED } from "../consts";
 
 const PUBLIC_ASTRO_BASE_PATH = process.env.PUBLIC_ASTRO_BASE_PATH;
 const basePath = PUBLIC_ASTRO_BASE_PATH || "";
-
+const visualEditingEnabled =
+  process.env.VISUAL_EDITING_ENABLED === "true" ||
+  process.env.PUBLIC_ASTRO_BASE_PATH === true;
 if (basePath && basePath !== "") {
   // Source and destination directories
 
-  fs.copyFileSync("./dist/en/404/index.html", "./dist/404.html");
+  if (!visualEditingEnabled) {
+    fs.copyFileSync("./dist/en/404/index.html", "./dist/404.html");
+  }
+
   if (!fs.existsSync("./build")) {
     fs.mkdirSync("./build");
   }
@@ -26,5 +32,7 @@ if (basePath && basePath !== "") {
     }
   });
 } else {
-  fs.copyFileSync("./dist/en/404/index.html", "./dist/404.html");
+  if (!visualEditingEnabled) {
+    fs.copyFileSync("./dist/en/404/index.html", "./dist/404.html");
+  }
 }
