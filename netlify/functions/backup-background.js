1937b1b2291683118c1447b0f7d9868c238f31ec
DavitAM <71630103+DavithkbY@users.noreply.github.com>
2024-10-21 20:55:33 +0200

    Feature/automatic backups (#170)
    * fixup! Add backup script
    
    * Add packages for backup script
    
    * Finish backup script initial version
    
    ---------
    
    Co-authored-by: HAKOBYAN, Davit <davit.hakobyan@acolad.com>

---
+++ netlify/functions/backup-background.js
@@ -0,0 +1,158 @@
+import "dotenv/config";
+import { createClient } from "@sanity/client";
+import exportDataset from "@sanity/export";
+import fs from "node:fs";
+import { schedule } from "@netlify/functions";
+
+import {
+  ShareServiceClient,
+  StorageSharedKeyCredential,
+} from "@azure/storage-file-share";
+
+const DATASET = process.env.SANITY_STUDIO_DATASET;
+
+const sanityClient = createClient({
+  projectId: process.env.SANITY_STUDIO_PROJECT_ID,
+  dataset: process.env.SANITY_STUDIO_DATASET,
+  useCdn: false, // set to `false` to bypass the edge cache
+  apiVersion: "2023-05-03", // use current date (YYYY-MM-DD) to target the latest API version
+  token: process.env.SANITY_API_READ_TOKEN,
+});
+
+const shareName = "fusebackups";
+const directoryName = "backups";
+
+const account = "amexiofusebackup";
+const accountKey = process.env.AZURE_FILES_ACCOUNT_KEY;
+
+const credential = new StorageSharedKeyCredential(account, accountKey);
+const serviceClient = new ShareServiceClient(
+  `https://${account}.file.core.windows.net`,
+  credential,
+);
+
+async function backup() {
+  console.log("export dataset");
+
+  await exportDataset({
+    // Instance of @sanity/client configured to correct project ID and dataset
+    client: sanityClient,
+
+    // Name of dataset to export
+    dataset: DATASET,
+
+    // Path to write zip-file to
+    outputPath: `/tmp/${DATASET}.tar.gz`,
+
+    assetConcurrency: 12,
+  });
+
+  const directoryClient = serviceClient
+    .getShareClient(shareName)
+    .getDirectoryClient(directoryName);
+
+  const readStream = fs.createReadStream(`/tmp/${DATASET}.tar.gz`, {
+    highWaterMark: 3000 * 1000,
+  });
+  let chunks = [];
+
+  readStream.on("data", async (chunk) => {
+    chunks.push(chunk); // Collect chunks
+  });
+
+  readStream.on("end", async () => {
+    const now = new Date()
+      .toISOString()
+      .replace("T", "_")
+      .replace(/:/g, "-")
+      .slice(0, 19);
+    const fileName = `${now}-${DATASET}.tar.gz`;
+    const fileClient = directoryClient.getFileClient(fileName);
+
+    const buffer = Buffer.concat(chunks);
+    const fileSize = Buffer.byteLength(buffer);
+    await fileClient.create(fileSize);
+    console.log(`Create file ${fileName} successfully`);
+
+    for (let i = 0; i < chunks.length; i++) {
+      await fileClient.uploadRange(
+        chunks[i],
+        i * 3000 * 1000,
+        Buffer.byteLength(chunks[i]),
+      );
+    }
+
+    console.log("Finished uploading backup to azure");
+
+    await deleteOldestZips();
+    // Upload file range
+  });
+
+  return true;
+}
+
+async function deleteOldestZips() {
+  try {
+    const directoryClient = serviceClient
+      .getShareClient(shareName)
+      .getDirectoryClient(directoryName);
+    let zipFiles = [];
+
+    // List files in the directory
+    let iter = directoryClient.listFilesAndDirectories();
+    for await (const item of iter) {
+      if (item.kind === "file" && item.name.endsWith(".gz")) {
+        const fileClient = directoryClient.getFileClient(item.name);
+        const properties = await fileClient.getProperties();
+
+        zipFiles.push({
+          name: item.name,
+          lastModified: properties.lastModified,
+        });
+      }
+    }
+
+    console.log(zipFiles);
+
+    // Sort by last modified date (oldest first)
+    zipFiles.sort((a, b) => a.lastModified - b.lastModified);
+
+    // If there are more than 5 zip files, delete the oldest ones
+    const filesToDelete =
+      zipFiles.length > 5 ? zipFiles.slice(0, zipFiles.length - 5) : [];
+
+    // Delete the extra files
+    for (const file of filesToDelete) {
+      const fileClient = directoryClient.getFileClient(file.name);
+      console.log(`Deleting file: ${file.name}`);
+      await fileClient.delete();
+    }
+
+    if (filesToDelete.length > 0) {
+      console.log(
+        `${filesToDelete.length} old zip file(s) deleted successfully.`,
+      );
+    } else {
+      console.log("No zip files to delete. The 5 newest zip files are kept.");
+    }
+  } catch (error) {
+    console.error("Error deleting files:", error.message);
+  }
+}
+
+const handler = async function (event, context, callback) {
+  backup()
+    .then(() => {
+      return {
+        statusCode: 200,
+      };
+    })
+    .catch((error) => {
+      return {
+        statusCode: 422,
+        body: `Something went wrong ${error}`,
+      };
+    });
+};
+
+exports.handler = schedule("*/2 * * * *", handler);
