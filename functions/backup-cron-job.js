6e6dc2c0bbf80105d8cd198d9659f2b8622c4eed
HAKOBYAN, Davit <davit.hakobyan@acolad.com>
2024-10-23 11:51:09 +0200

    Add catch clausule

--- functions/backup-cron-job.js
+++ functions/backup-cron-job.js
@@ -1,20 +1,33 @@
 import "dotenv/config";
 import { schedule } from "@netlify/functions";
 
-const handler = function (event, context, callback) {
+const handler = async function (event, context, callback) {
   const url = `${process.env.ASTRO_SITE_URL}.netlify/functions/backup-background`; // Replace with the actual endpoint
 
   const data = {
     password: process.env.BACKUP_FUNCTION_PASSWORD,
   };
 
-  fetch(url, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(data),
-  });
+  try {
+    const response = await fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    });
+
+    // Ensure the request was successful
+    if (!response.ok) {
+      throw new Error(`Request failed with status ${response.status}`);
+    }
+  } catch (error) {
+    console.error("Error during fetch:", error);
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: "Function failed" }),
+    };
+  }
 
   return {
     statusCode: 200,
