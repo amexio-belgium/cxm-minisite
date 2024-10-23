3daa3f88e7a5919f3ac10e6deb31453c1e7c9b1d
HAKOBYAN, Davit <davit.hakobyan@acolad.com>
2024-10-23 22:51:39 +0200

    Add prebuild script

--- src/scripts/postbuild.js
+++ src/scripts/postbuild.js
@@ -30,18 +30,6 @@ if (backupsEnabled) {
   );
 }
 
-fs.writeFileSync(
-  "./public/_redirects",
-  `https://www.${new URL(process.env.ASTRO_SITE_URL).hostname}/* https://${new URL(process.env.ASTRO_SITE_URL).hostname}/:splat 301!\n` +
-    "/ /en/ 301!\n" +
-    "/:lang/* /:lang/404/ 404",
-);
-// Script to create a robots.txt with the current sitemap
-fs.writeFileSync(
-  "./public/robots.txt",
-  `Sitemap: ${process.env.ASTRO_SITE_URL}sitemap-index.xml`,
-);
-
 if (basePath && basePath !== "") {
   // Source and destination directories
 
---
+++ src/scripts/prebuild.js
@@ -0,0 +1,14 @@
+import fs from "node:fs";
+import "dotenv/config";
+
+fs.writeFileSync(
+  "./public/_redirects",
+  `https://www.${new URL(process.env.ASTRO_SITE_URL).hostname}/* https://${new URL(process.env.ASTRO_SITE_URL).hostname}/:splat 301!\n` +
+    "/ /en/ 301!\n" +
+    "/:lang/* /:lang/404/ 404",
+);
+// Script to create a robots.txt with the current sitemap
+fs.writeFileSync(
+  "./public/robots.txt",
+  `Sitemap: ${process.env.ASTRO_SITE_URL}sitemap-index.xml`,
+);
