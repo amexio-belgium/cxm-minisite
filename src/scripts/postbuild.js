8d77ee1b7d591f12a0d8bbfa8e3243bc5cca9c08
HAKOBYAN, Davit <davit.hakobyan@acolad.com>
2024-10-23 22:46:36 +0200

    Add redirects script

--- src/scripts/postbuild.js
+++ src/scripts/postbuild.js
@@ -30,6 +30,12 @@ if (backupsEnabled) {
   );
 }
 
+fs.writeFileSync(
+  "./public/_redirects",
+  `https://www.${new URL(process.env.ASTRO_SITE_URL).hostname}/* https://${new URL(process.env.ASTRO_SITE_URL).hostname}/:splat 301!\n` +
+    "/ /en/ 301!\n" +
+    "/:lang/* /:lang/404/ 404",
+);
 // Script to create a robots.txt with the current sitemap
 fs.writeFileSync(
   "./public/robots.txt",
